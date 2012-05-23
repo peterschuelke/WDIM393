d93e607039fc7349427f68ab127630a4e1e83045
Peter Schuelke <peter.c.schuelke@gmail.com>
2012-05-23 12:08:48 -0700

    pushing everything

---
+++ WDIM393/js/test.js
@@ -0,0 +1,4 @@
+Modernizr.load({
+  test: Modernizr.textshadow,
+  nope: ['js/jquery.textshadow.js','js/jquery.textshadow.css','js/thisMakesNoSense.js']
+});
---
+++ WDIM393/js/thisMakesNoSense.js
@@ -0,0 +1,1 @@
+$('h1').textshadow('10px 10px 10px #999');
